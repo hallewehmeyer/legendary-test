1bead028a59359f8a83dcf4917a125a527008fb6
Halle Wehmeyer <hallewehmeyer@Halles-MBP.attlocal.net>
2021-03-15 09:10:18 -0500

    JavaScript functions added

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$('.box-explore').hide();
+
+function moreDetails() {
+    $('.title-heading').hide();
+    $('.box-explore').show();
+}
+
+function closeButton() {
+    $('.title-heading').show();
+    $('.box-explore').hide();
+}
